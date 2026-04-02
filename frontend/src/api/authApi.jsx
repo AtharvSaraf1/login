a047620d0129f233cf4c2541e054c86d1904260e
AtharvSaraf1 <142170446+AtharvSaraf1@users.noreply.github.com>
2026-04-02 22:29:09 +0530

    Change API base URL to production endpoint

--- frontend/src/api/authApi.jsx
+++ frontend/src/api/authApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = axios.create({
-    baseURL: 'http://localhost:3000/api/auth',
+    baseURL: 'https://login-ilde.onrender.com/',
 })
 
 export const register=async(formData)=>{
